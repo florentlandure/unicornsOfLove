90addd28759273e3444c1a7745289c025eb64fba
Angular CLI <angular-cli@angular.io>
2017-05-20 14:35:38 +0200

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { UnicornsOfLovePage } from './app.po';
+
+describe('unicorns-of-love App', () => {
+  let page: UnicornsOfLovePage;
+
+  beforeEach(() => {
+    page = new UnicornsOfLovePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
